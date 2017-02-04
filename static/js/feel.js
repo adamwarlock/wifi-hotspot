cc0810265dd2195e8c979c53703b573bcb8625cc
vivek <vivekchopra42@outlook.com>
2017-02-04 18:54:38 +0530

    alot of changes

--- static/js/feel.js
+++
@@ -1,9 +0,0 @@
-angular.module('ngLogin', [])
-
-.controller('btnCtrl', function($scope) {
-  /*  $scope.exp1 = false;
-    $scope.exp2 = false;*/
-    $scope.enable = function() {
-        return !($scope.exp1 && $scope.exp2);
-    };
-})
